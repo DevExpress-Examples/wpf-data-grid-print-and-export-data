021602132af1148721d5a782a3f3aeec204b411f
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-08 15:19:10 +0400

    Update example

--- CS/IssueList.cs
+++ CS/IssueList.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 
 namespace DXGrid_PrintGrid {
     public class IssueList {
-        public List<IssueDataObject> List { get; private set; }
-        public IssueList() {
-            List = GetIssueList();
-        }
-        List<IssueDataObject> GetIssueList() {
-            List<IssueDataObject> data = new List<IssueDataObject>();
-            data.Add(new IssueDataObject() { IssueName = "Transaction History", IssueType = "Bug", IsPrivate = true });
-            data.Add(new IssueDataObject() { IssueName = "Ledger: Inconsistency", IssueType = "Bug", IsPrivate = false });
-            data.Add(new IssueDataObject() { IssueName = "Data Import", IssueType = "Request", IsPrivate = false });
-            data.Add(new IssueDataObject() { IssueName = "Data Archiving", IssueType = "Request", IsPrivate = true });
+        public static ObservableCollection<IssueData> GetIssues() {
+            ObservableCollection<IssueData> data = new ObservableCollection<IssueData>();
+            data.Add(new IssueData() { IssueName = "Transaction History", IssueType = "Bug", IsPrivate = true });
+            data.Add(new IssueData() { IssueName = "Ledger: Inconsistency", IssueType = "Bug", IsPrivate = false });
+            data.Add(new IssueData() { IssueName = "Data Import", IssueType = "Request", IsPrivate = false });
+            data.Add(new IssueData() { IssueName = "Data Archiving", IssueType = "Request", IsPrivate = true });
             return data;
         }
     }
-    public class IssueDataObject {
+    public class IssueData {
         public string IssueName { get; set; }
         public string IssueType { get; set; }
         public bool IsPrivate { get; set; }
--- CS/Window1.xaml.cs
+++ CS/Window1.xaml.cs
@@ -1,25 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Documents;
-using DevExpress.Xpf.Grid;
-using DevExpress.Xpf.Printing;
+﻿using System.Windows;
 
 namespace DXGrid_PrintGrid {
     public partial class Window1 : Window {
         public Window1() {
             InitializeComponent();
-            DataContext = new IssueList();
+            grid.ItemsSource = IssueList.GetIssues();
         }
-        private void PreviewGrid(object sender, RoutedEventArgs e) {
+        void PreviewGrid(object sender, RoutedEventArgs e) {
             view.ShowPrintPreviewDialog(this);
         }
-        private void ExportToXls(object sender, RoutedEventArgs e) {
+        void ExportToXls(object sender, RoutedEventArgs e) {
             view.ExportToXlsx(@"d:\grid_export.xlsx");
         }
-        private void ExportToCsv(object sender, RoutedEventArgs e) {
+        void ExportToCsv(object sender, RoutedEventArgs e) {
             view.ExportToCsv(@"d:\grid_export.csv");
         }
-        private void ExportToPng(object sender, RoutedEventArgs e) {
+        void ExportToPng(object sender, RoutedEventArgs e) {
             view.ExportToImage(@"d:\grid_export.png");
         }
     }
