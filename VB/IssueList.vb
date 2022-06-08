b3ddad6088a60d8e9bb25f01cf1895c06974a09e
DevExpressExampleBot <fakeEmail>
2022-06-08 15:26:08 +0400

    Source auto update [skip ci]

--- VB/IssueList.vb
+++ VB/IssueList.vb
@@ -1,34 +1,25 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+Imports System.Collections.ObjectModel
 
 Namespace DXGrid_PrintGrid
+
     Public Class IssueList
-        Private privateList As List(Of IssueDataObject)
-        Public Property List() As List(Of IssueDataObject)
-            Get
-                Return privateList
-            End Get
-            Private Set(ByVal value As List(Of IssueDataObject))
-                privateList = value
-            End Set
-        End Property
-        Public Sub New()
-            List = GetIssueList()
-        End Sub
-        Private Function GetIssueList() As List(Of IssueDataObject)
-            Dim data As New List(Of IssueDataObject)()
-            data.Add(New IssueDataObject() With {.IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
-            data.Add(New IssueDataObject() With {.IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
-            data.Add(New IssueDataObject() With {.IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
-            data.Add(New IssueDataObject() With {.IssueName = "Data Archiving", .IssueType = "Request", .IsPrivate = True})
+
+        Public Shared Function GetIssues() As ObservableCollection(Of IssueData)
+            Dim data As ObservableCollection(Of IssueData) = New ObservableCollection(Of IssueData)()
+            data.Add(New IssueData() With {.IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
+            data.Add(New IssueData() With {.IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
+            data.Add(New IssueData() With {.IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
+            data.Add(New IssueData() With {.IssueName = "Data Archiving", .IssueType = "Request", .IsPrivate = True})
             Return data
         End Function
     End Class
-    Public Class IssueDataObject
-        Public Property IssueName() As String
-        Public Property IssueType() As String
-        Public Property IsPrivate() As Boolean
+
+    Public Class IssueData
+
+        Public Property IssueName As String
+
+        Public Property IssueType As String
+
+        Public Property IsPrivate As Boolean
     End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,28 +1,29 @@
-﻿Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Documents
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Printing
 
 Namespace DXGrid_PrintGrid
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            DataContext = New IssueList()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = IssueList.GetIssues()
         End Sub
+
         Private Sub PreviewGrid(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            view.ShowPrintPreviewDialog(Me)
+            Me.view.ShowPrintPreviewDialog(Me)
         End Sub
+
         Private Sub ExportToXls(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            view.ExportToXlsx("d:\grid_export.xlsx")
+            Me.view.ExportToXlsx("d:\grid_export.xlsx")
         End Sub
+
         Private Sub ExportToCsv(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            view.ExportToCsv("d:\grid_export.csv")
+            Me.view.ExportToCsv("d:\grid_export.csv")
         End Sub
+
         Private Sub ExportToPng(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            view.ExportToImage("d:\grid_export.png")
+            Me.view.ExportToImage("d:\grid_export.png")
         End Sub
     End Class
 End Namespace
