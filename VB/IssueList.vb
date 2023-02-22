04ef647fd8a4c50710ca96163e2f28d5a870b88e
DevExpressExampleBot <fakeEmail>
2023-02-22 19:46:31 +0400

    Source auto update [skip ci]

--- VB/IssueList.vb
+++ VB/IssueList.vb
@@ -1,24 +1,27 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 
 Namespace DXGrid_PrintGrid
+
     Public Class IssueList
-        Private privateList As List(Of IssueDataObject)
-        Public Property List() As List(Of IssueDataObject)
+
+        Private _List As List(Of DXGrid_PrintGrid.IssueDataObject)
+
+        Public Property List As List(Of IssueDataObject)
             Get
-                Return privateList
+                Return _List
             End Get
+
             Private Set(ByVal value As List(Of IssueDataObject))
-                privateList = value
+                _List = value
             End Set
         End Property
+
         Public Sub New()
             List = GetIssueList()
         End Sub
+
         Private Function GetIssueList() As List(Of IssueDataObject)
-            Dim data As New List(Of IssueDataObject)()
+            Dim data As List(Of IssueDataObject) = New List(Of IssueDataObject)()
             data.Add(New IssueDataObject() With {.IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
             data.Add(New IssueDataObject() With {.IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
             data.Add(New IssueDataObject() With {.IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
@@ -26,9 +29,13 @@ Namespace DXGrid_PrintGrid
             Return data
         End Function
     End Class
+
     Public Class IssueDataObject
-        Public Property IssueName() As String
-        Public Property IssueType() As String
-        Public Property IsPrivate() As Boolean
+
+        Public Property IssueName As String
+
+        Public Property IssueType As String
+
+        Public Property IsPrivate As Boolean
     End Class
 End Namespace
